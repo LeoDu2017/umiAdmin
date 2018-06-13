cb77b4820ef2d2bbba274e525cc53654332e7e51
LeoDu2017 <2516849822@qq.com>
2018-06-13 21:36:27 +0800

    Albums 图片的请求与加载

--- src/components/units/index.js
+++ src/components/units/index.js
@@ -1,9 +1,5 @@
 import Header from './Album-header';
-import Randeritems from './Randeritems';
 import Pictures from './Album-imgs';
-import Pic_actions from './Album-actions';
-import Controls from './Album-controls';
 import Tree from './Album-tree';
-import Tree_actions from './Album-treeaction';
 
-export {Header,Randeritems,Pictures,Pic_actions,Controls,Tree,Tree_actions}
+export {Header,Pictures,Tree}
