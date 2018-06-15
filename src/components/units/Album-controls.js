354c41c30759679f82a4956b0237270690fafc20
LeoDu2017 <2516849822@qq.com>
2018-06-15 21:45:43 +0800

    Albums 图片组件翻页成功

--- src/components/units/Album-controls.js
+++ src/components/units/Album-controls.js
@@ -1,23 +1,31 @@
 import intl from 'react-intl-universal';
-import {Col,Button} from 'antd';
+import {Col,Button,Pagination} from 'antd';
 import styles from 'styles/components.less';
+import {getPictures} from 'actions/pictures';
 
-const Controls = ()=>(
+const Controls = ({dispatch,current,total})=>(
   <Col className={styles.ctrls}>
+
     <Button
       className={styles.premaryBtn}> {intl.get('USESELECTED')} </Button>
-    <Col className={styles.pages}>
-      <Button
-        icon="double-left"
-        disabled="true"
-        className={styles.previous}> </Button>
-      <Button
-        className={`${styles.page} ${styles.current}`}>1</Button>
-      <Button
-        className={styles.page}>2</Button>
-      <Button
-        icon="double-right" className={styles.next}> </Button>
-    </Col>
+    <Pagination
+      className="ant-table-pagination"
+      total={total}
+      current={current}
+      onChange={ getPictures.bind(null,dispatch) }
+    />
+    {/*<Col className={styles.pages}>*/}
+      {/*<Button*/}
+        {/*icon="double-left"*/}
+        {/*disabled="true"*/}
+        {/*className={styles.previous}> </Button>*/}
+      {/*<Button*/}
+        {/*className={`${styles.page} ${styles.current}`}>1</Button>*/}
+      {/*<Button*/}
+        {/*className={styles.page}>2</Button>*/}
+      {/*<Button*/}
+        {/*icon="double-right" className={styles.next}> </Button>*/}
+    {/*</Col>*/}
   </Col>
 );
 
