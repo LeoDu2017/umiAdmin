facb546b4a36c52fa072c30bc84498f1f9195706
LeoDu2017 <2516849822@qq.com>
2018-06-19 17:45:01 +0800

    Albums 图册组件选取图片

--- src/actions/albums.js
+++ src/actions/albums.js
@@ -5,3 +5,18 @@ export function showAlbums(dispatch,boolenValue){
     payload:dispaly
   })
 }
+
+export function selectImgs(dispatch,id,type){
+  dispatch({
+    type:'pictures/setSelectImgs',
+    payload:{id,type}
+  })
+}
+
+export function removeSelected(dispatch,id,event){
+  event.stopPropagation();
+  dispatch({
+    type:'pictures/removeSelectImgs',
+    payload:id
+  })
+}
