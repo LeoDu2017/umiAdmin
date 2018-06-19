e72aa0a85dcebf9adeb3be56aa777023104895d1
LeoDu2017 <2516849822@qq.com>
2018-06-19 17:45:31 +0800

    Albums 图册组件选取图片

--- src/components/units/Album-controls.js
+++ src/components/units/Album-controls.js
@@ -2,31 +2,16 @@ import intl from 'react-intl-universal';
 import {Col,Button,Pagination} from 'antd';
 import styles from 'styles/components.less';
 import {getPictures} from 'actions/pictures';
-
-const Controls = ({dispatch,current,total})=>(
+const Controls = ({dispatch,current,total,length})=>(
   <Col className={styles.ctrls}>
-
     <Button
-      className={styles.premaryBtn}> {intl.get('USESELECTED')} </Button>
+      className={styles.premaryBtn}> {intl.get('USESELECTED')} ({length}) </Button>
     <Pagination
-      className="ant-table-pagination"
+      className={styles.pages}
       total={total}
       current={current}
       onChange={ getPictures.bind(null,dispatch) }
     />
-    {/*<Col className={styles.pages}>*/}
-      {/*<Button*/}
-        {/*icon="double-left"*/}
-        {/*disabled="true"*/}
-        {/*className={styles.previous}> </Button>*/}
-      {/*<Button*/}
-        {/*className={`${styles.page} ${styles.current}`}>1</Button>*/}
-      {/*<Button*/}
-        {/*className={styles.page}>2</Button>*/}
-      {/*<Button*/}
-        {/*icon="double-right" className={styles.next}> </Button>*/}
-    {/*</Col>*/}
   </Col>
 );
-
 export default Controls;
